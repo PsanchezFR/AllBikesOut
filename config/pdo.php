f8e8a40561c745b1df87cdf946b656e8e30f76aa
Paul Sanchez <p.sanchez.fr@gmail.com>
2018-07-12 16:15:53 +0200

    changed pdo

--- config/pdo.php
+++ config/pdo.php
@@ -1,6 +1,6 @@
 <?php
     try{
-        $bdd = new PDO('mysql:host=localhost;dbname=lms_simplon', 'root', '150389ps');
+        $bdd = new PDO('mysql:host=localhost;dbname=', 'root', '');
     }
     catch(PDOException $e){
         $logger->log($e->getMessage());
