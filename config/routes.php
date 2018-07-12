cd1b8ad796c78f78f1c55b25e1448ba457f0b404
Paul Sanchez <p.sanchez.fr@gmail.com>
2018-07-12 16:12:53 +0200

    Added controllers and routes.

--- config/routes.php
+++ config/routes.php
@@ -3,7 +3,15 @@
     //
     //  HOME
     //
-		'/home' => 'controller/home.php',
+		'/' => 'controller/home.php',
+
+    //
+    //  APP VIEW
+    //
+        '/broken' => 'pages/broken.php',
+        '/lost' => 'pages/lost.php',
+        '/search' => 'pages/search.php',
+        '/show' => 'pages/show.php',
 	];
 
 ?>
