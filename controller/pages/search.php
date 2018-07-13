0a581d5684db90be407bcc6343b3e5b76f44ba3a
Paul Sanchez <p.sanchez.fr@gmail.com>
2018-07-13 11:58:00 +0200

    Change main search script.

--- controller/pages/search.php
+++ controller/pages/search.php
@@ -8,21 +8,25 @@
 </form>
 <div class="border col-11 col-md-5 mx-auto h-90">
     <?php
-        $request = $bdd->prepare("SELECT id_velo FROM ve_to_de");
+        $request = $bdd->prepare("SELECT id_velo FROM ve_to_de WHERE :searchInput LIKE '%' + id_velo + '%'");
+        $request->bindParam('searchInput', $_POST['search'], PDO::PARAM_STR);
         $request->execute();
-        $USER = $request->fetchObject('model\User');
+        $velos = $request->fetchAll(PDO::FETCH_CLASS, 'model\ve_to_de');
 
-    //    <div class="border col-auto d-flex align-items-center " style="min-height: 40px">
-    //            <i class="fas fa-bicycle col-2 fa-2x"></i>
-    //            <p class="m-0 col-6">1020</p>
-    //            <div class="col-3">
-    //                <i class="fas fa-star "></i>
-    //                <i class="fas fa-star"></i>
-    //                <i class="fas fa-star-half-alt"></i>
-    //                <i class="far fa-star"></i>
-    //                <i class="far fa-star"></i>
-    //            </div>
-    //        <i class="fas fa-chevron-right col-1 fa-2x"></i>
-    //    </div>
+        foreach ($velos as $key => $value){
+                echo "<div class='border col-auto d-flex align-items-center' style='min-height: 40px'>";
+                echo "<i class='fas fa-bicycle col-2 fa-2x'></i>";
+                echo "<p class='m-0 col-6'>$value.id_velo</p>";
+                echo "<div class='col-3'>";
+                echo "<i class='fas fa-star '></i>";
+                echo "<i class='fas fa-star'></i>";
+                echo "<i class='fas fa-star-half-alt'></i>";
+                echo "<i class='far fa-star'></i>";
+                echo "<i class='far fa-star'></i>";
+                echo "</div>";
+                echo "<i class='fas fa-chevron-right col-1 fa-2x'></i>";
+                echo "</div>";
+
+        }
            ?>
 </div>
